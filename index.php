c0b27800729d9bbe4de49b6cf0220b10ce840ffb
Daniela Sánchez H. <133073080+DanSanchezHerrera@users.noreply.github.com>
2026-05-05 11:42:47 -0400

    Index

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        <link rel="stylesheet" href="css/estilos.css">
+        <link rel="stylesheet" href="css/navbar.css">
+        <title>Inicio - PequeMundo</title>
+    </head>
+    <body>
+        <?php include 'masterpage/menu.php'; ?>
+        <main class="container my-5">
+            <section class="row align-items-center mb-5">
+                <div class="col-md-4 text-center">
+                    <img src="img/pequeMundo_icono2.png" alt="Logo Pequemundo" class="imagen-home">
+                </div>
+                <div class="col-md-8">
+                    <div class="bg-white p-5 rounded shadow-sm text-center">
+                        <h2>Los mejores muebles para tu bebé.</h2>
+                        <p>Conoce nuestras opciones y descuentos. Retira en tienda o pide despacho a domicilio.</p>
+                        <a class="btn btn-custom" href="catalogo.php">Catálogo</a>
+                    </div>
+                </div>
+            </section>
+            <section class="row mb-5">
+                <div class="col-12">
+                    <img src="img/cuna.png" alt="Cuna Bebé" class="img-fluid rounded">
+                </div>
+            </section>
+            <h2>Sobre Nosotros</h2>
+            <section class="row mb-5 p-3 bg-white rounded shadow-sm">
+                <div class="col-12">
+                    <p>PequeMundo es un emprendimiento chileno dedicado al diseño y venta de muebles infantiles, enfocado en crear espacios seguros y acogedores para los más pequeños. Nos especializamos en productos funcionales y de calidad que acompañan el crecimiento de cada niño.</p>
+                    <div class="text-end">
+                        <a class="btn btn-custom " href="contacto.php">Conocer Más</a>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <?php include 'masterpage/footer.php'; ?>
+    </body>
+</html>
