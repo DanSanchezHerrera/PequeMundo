96668122cd6bfd16682af13795de567d15700611
Daniela Sánchez H. <133073080+DanSanchezHerrera@users.noreply.github.com>
2026-05-05 12:05:00 -0400

    Registro base

---
+++ registro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        <link rel="stylesheet" href="css/estilos.css">
+        <link rel="stylesheet" href="css/navbar.css">
+        <title>Registro - PequeMundo</title>
+    </head>
+    <body>
+        <?php include 'masterpage/menu.php'; ?>
+        <main class="container my-5">
+        </main>
+        <?php include 'masterpage/footer.php'; ?>
+    </body>
+</html>
