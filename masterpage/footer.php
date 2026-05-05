11babb325f97d2104cecf08e933271325a8a8069
Daniela Sánchez H. <133073080+DanSanchezHerrera@users.noreply.github.com>
2026-05-05 11:45:10 -0400

    Index listo

---
+++ masterpage/footer.php
@@ -0,0 +1,18 @@
+<footer class="text-center" style="background-color: rgba(252, 219, 126);">
+    <div class="container pt-4">
+        <section class="mb-4">
+            <a class="btn btn-link btn-lg footer-icon m-1" href="#" role="button">
+                <i class="fa-brands fa-facebook-f"></i>
+            </a>
+            <a class="btn btn-link btn-lg footer-icon m-1" href="#" role="button">
+                <i class="fa-brands fa-x-twitter"></i>
+            </a>
+            <a class="btn btn-link btn-lg footer-icon m-1" href="#" role="button">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+        </section>
+    </div>
+    <div class="text-center p-3 text-custom" style="background-color: rgb(248, 215, 123);">
+        © 2026 PequeMundo:
+    </div>
+</footer>
---
+++ masterpage/menu.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-custom">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">
+      <img src="img/pequeMundo_logo.png" alt="Logo" width="130" height="65" class="d-inline-block align-text-top">
+    </a>
+    <ul class="navbar-nav ms-auto me-4 mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="index.php">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Catálogo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Contacto</a>
+      </li>
+    </ul>
+    <div class="d-flex">
+      <a class="btn btn-custom me-2" href="../iniciar_sesion.php">Iniciar Sesión</a>
+    </div>
+  </div>
+</nav>
