23c8b6d0ba9eaf308776993f9667de9d3d8ae3b4
Daniela Sánchez H. <133073080+DanSanchezHerrera@users.noreply.github.com>
2026-05-12 12:08:31 -0400

    Action corregido

--- action/usuario_action.php
+++ action/usuario_action.php
@@ -1,8 +1,8 @@
 <?php
-    // importar la conexión
+    // importar conexión
     require_once "../config/conexion.php";
-	
-    // procesar la solicitud de registrar
+
+    // procesar registro
     if(isset($_POST["btnRegistrar"])){
 
         $nombre = $_POST["nombre"];
@@ -11,26 +11,26 @@
         $telefono = $_POST["telefono"];
         $tipo_usuario = $_POST["tipo_usuario"];
 
-        // encriptar la contraseña
+        // encriptar contraseña
         $pass_enc = password_hash($password, PASSWORD_DEFAULT);
 
         // consulta SQL
-        $sql = "INSERT INTO cliente (nombre, apellido, password, telefono, tipo_usuario)
-                VALUES ('$nombre', '$apellido', '$pass_enc', '$telefono', '$tipo_usuario')";
+        $sql = "INSERT INTO usuario(nombre, apellido, password, telefono, tipo_usuario)
+                VALUES('$nombre', '$apellido', '$pass_enc', '$telefono', '$tipo_usuario')";
 
-        // guardar en la base de datos
+        // ejecutar consulta
         $resultado = mysqli_query($conexion, $sql);
 
         // validar resultado
         if($resultado){
             echo "<script>
-                    alert('Cliente registrado correctamente');
-                    window.location.href = '../index.php';
+                    alert('Usuario registrado correctamente');
+                    window.location.href='../index.php';
                   </script>";
         }else{
             echo "<script>
-                    alert('Error al registrar el cliente');
-                    window.location.href = '../index.php';
+                    alert('Error al registrar el usuario');
+                    window.location.href='../index.php';
                   </script>";
         }
     }
