649a24cd2c7a37658a19784bfa2c99ed02c71ab6
Daniela Sánchez H. <133073080+DanSanchezHerrera@users.noreply.github.com>
2026-05-12 12:27:17 -0400

    Update iniciar_sesion.php

--- iniciar_sesion.php
+++ iniciar_sesion.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión</title>
-    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+     <link rel="stylesheet" href="css/navbar.css">
 </head>
 <body>
     <?php include 'masterpage/menu.php'; ?>
